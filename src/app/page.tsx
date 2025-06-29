c374a0a2c6741a2bd45a3168faf1e16830700873
SarwarSarker <sarwarsarker00@gmail.com>
2025-06-29 12:27:00 +0600

    error fixed in app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
-import Skills from "@/components/Skills";
+// import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
